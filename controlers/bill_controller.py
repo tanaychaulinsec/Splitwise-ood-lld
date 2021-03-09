562bd56b31606b2016590c03a5f06bf8119d6a75
Tanay Chauli <30913633+tanaychaulinsec@users.noreply.github.com>
2021-03-09 20:03:38 +0530

    fix variable naming issues

--- controlers/bill_controller.py
+++ controlers/bill_controller.py
@@ -1,14 +1,14 @@
 class BillController(object):
-    def __init__(self, billService):
-        self.billService = billService
+    def __init__(self, billServices):
+        self.billServices = billServices
 
     def addBill(self, id, groupId, amount, contribution, paidBy):
-        return self.billService.addBill(id, groupId, amount, contribution, paidBy)
+        return self.billServices.addBill(id, groupId, amount, contribution, paidBy)
 
     def getUserBalance(self, userId):
         balance = 0
-        for bilId in self.billService.billDetails:
-            bill = self.billService.billDetails.get(bilId)
+        for bilId in self.billServices.billDetails:
+            bill = self.billServices.billDetails.get(bilId)
             if userId in bill.getContribution():
                 balance -= bill.getContribution().get(userId)
             if userId in bill.getPaidby():
