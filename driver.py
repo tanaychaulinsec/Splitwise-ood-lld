6f633d7043b5450c6e4a7c4e632d81edc8f4804f
Tanay Chauli <30913633+tanaychaulinsec@users.noreply.github.com>
2021-03-09 20:09:14 +0530

    Show details of Bill and each user available balance

--- driver.py
+++ driver.py
@@ -20,11 +20,16 @@ user5 = userController.addUser('user5','manoj')
 members = [user1,user2,user3,user4,user5]
 group1 = groupController.addGroup('g001','megapolis',members)
 
-print (group1.getMembers())
-paidBy = {'user1':150,'user2':100,'user3':100,'user4':50,'user5':100}
-contribution = {'user1':100,'user2':100,'user3':100,'user4':100,'user5':100}
+#print (group1.getMembers())
+paidBy={'user1':50,'user2':250,'user3':200,'user4':300,'user5':100}
+contribution={'user1':500,'user2':100,'user3':100,'user4':100,'user5':100}
 
-bill1 = billController.addBill('bill1','group1',500,contribution,paidBy)
-
-balance = billController.getUserBalance('user1')
-# print (balance)
+bill = billController.addBill('bill1','group1',900,contribution,paidBy)
+invoice={}
+for user in contribution.keys():
+    balance= billController.getUserBalance(user)
+    invoice[user]=balance
+print("Expense of this month is\n")
+for userName , status in invoice.items():
+    print ( userName+":" + status)
+print("\n")
